e469db96c0ce2b11c22be263345e8507eb944055
MintYiqingchen <821244608@qq.com>
2018-03-26 23:58:29 +0800

    roughly finish seq2seq

--- script/seq2seq.py
+++ script/seq2seq.py
@@ -7,7 +7,10 @@ import numpy as np
 known, vocabs, chars = pickle.load(open('vocabs.pkl','rb'))
 
 myConfig = {
+        'save_name':'seq2seq',
+        'output_dir':'v1',
         'max_epoch':70000,
+        'epoch_size': 20000,
         'batchsize':256,
         'lr':0.1,
         'wg_dim':known,
@@ -20,42 +23,27 @@ myConfig = {
 
 # ============ layers ==============
 def GloveEmbed():
+    # load parameters
+    npglove = np.zeros((myConfig['wg_dim'], myConfig['embed_dim']), dtype=np.float32)
+    with open('glove.6B.300d.txt',encoding='utf8') as f:
+        for line in f:
+            parts = line.split()
+            word = parts[0].lower()
+            idx = vocabs[word]
+            if idx<known:
+                npglove[idx] = np.array([float(p) for p in parts[1:]])
+    glove = C.constant(npglove)
+    nonglove = C.parameter(shape=(myConfig['wn_dim'], myConfig['embed_dim']), init = C.glorot_uniform(), name='nongloveE')
+    @C.Function
     def embed_layer(awk, awn):
-        # load parameters
-        npglove = np.zeros((myConfig['wg_dim'], myConfig['embed_dim']), dtype=np.float32)
-        with open('glove.6B.300d.txt',encoding='utf8') as f:
-            for line in f:
-                parts = line.split()
-                word = parts[0].lower()
-                idx = vocabs[word]
-                if idx<known:
-                    npglove[idx] = np.array([float(p) for p in parts[1:]])
-        glove = C.constant(npglove)
-        nonglove = C.parameter(shape=(myConfig['wn_dim'], myConfig['embed_dim']), init = C.glorot_uniform(), name='nongloveE')
-
         a_processed = C.times(awk, glove) + C.times(awn, nonglove)
-
         return a_processed
     return embed_layer
 
-
-def create_model(is_train=True):
+def create_model():
     '''
-    return:model, placeholder
+    return:model
     '''
-    q = C.Axis.new_unique_dynamic_axis('q')
-    a = C.Axis.new_unique_dynamic_axis('a')
-    b = C.Axis.default_batch_axis()
-    qwk = C.sequence.input_variable(myConfig['wg_dim'], sequence_axis = q, is_sparse = False, name='qwk')
-    qwn = C.sequence.input_variable(myConfig['wn_dim'], sequence_axis = q, is_sparse = False, name='qwn')
-    awk = C.sequence.input_variable(myConfig['wg_dim'], sequence_axis = a, is_sparse = False, name='awk')
-    awn = C.sequence.input_variable(myConfig['wn_dim'], sequence_axis = a, is_sparse = False, name='awn')
-
-    input_ph = {'qwk':qwk,'qwn':qwn,'awk':awk,'awn':awn}
-    embed_layer = GloveEmbed()
-
-    a_processes = embed_layer(awk, awn)
-
     with default_options(enable_self_stabilization=True):
         encoder = Sequential([
             Stabilizer(),
@@ -68,7 +56,7 @@ def create_model(is_train=True):
         stab_out = Stabilizer()
         attention_model = AttentionModel(myConfig['attention_dim'])
         rec_blocks = [LSTM(myConfig['hidden_dim']) for _ in range(myConfig['layers'])]
-        proj_out = Dense(myConfig['wg_dim']+myConfig['wn_dim'])
+        proj_out = Dense(myConfig['wg_dim']+myConfig['wn_dim'], input_rank=1)
 
         rec = Recurrence(rec_blocks[0])
         # last
@@ -80,21 +68,106 @@ def create_model(is_train=True):
             Label('out_proj_out')
             ])
 
-        def decoder(his, inp):
+        def decoder(history, inp):
+            # encode
+            encode_h, encode_c = encoder(inp).outputs
+            # decode prepare
+            his_stab = stab_in(history)
             # first layer
+            out_fwd = ForwardDeclaration()
             attn = attention_model(encode_h, out_fwd)
-            rec_in = C.splice(a_stab, attn)
+            rec_in = C.splice(his_stab, attn)
             rec_out = rec(rec_in) # output
             rec_out_past = C.sequence.past_value(rec_out)
             out_fwd.resolve_to(rec_out_past)
-            out_proj_out = last_2_layer(rec_out)
+
+            rec_stab = stab_out(rec_out)
+            out_proj_out = last_2_layer(rec_stab)
             return out_proj_out
 
-    return decoder, input_ph
+    return decoder
 
-def create_train_model(s2smodel):
-    def train_model(inp_wk, inp_wn, label_wk, label_wn):
-        past_label =
+def create_train_model(s2smodel, embed_layer):
+    '''
+    return: @input map @softmax @loss
+    '''
+    q = C.Axis.new_unique_dynamic_axis('q')
+    a = C.Axis.new_unique_dynamic_axis('a')
+    b = C.Axis.default_batch_axis()
+    qwk = C.sequence.input_variable(myConfig['wg_dim'], sequence_axis = q, is_sparse = False, name='qwk')
+    qwn = C.sequence.input_variable(myConfig['wn_dim'], sequence_axis = q, is_sparse = False, name='qwn')
+    awk = C.sequence.input_variable(myConfig['wg_dim'], sequence_axis = a, is_sparse = False, name='awk')
+    awn = C.sequence.input_variable(myConfig['wn_dim'], sequence_axis = a, is_sparse = False, name='awn')
+
+    input_ph = {'qwk':qwk,'qwn':qwn,'awk':awk,'awn':awn}
+    a_processed = embed_layer(awk, awn)
+    q_processed = embed_layer(qwk, qwn)
+    q_onehot = C.splice(qwk, qwn)
+
+    logits = s2smodel(q_processed, a_processed)
+
+    labels = C.sequence.slice(q_onehot,1, 0) # <s> a b c </s> -> a b c </s>
+    logits = C.reconcile_dynamic_axes(logits, labels)
+    loss = C.cross_entropy_with_softmax(logits, labels)
+    errs = C.classification_error(logits, labels)
+    return input_ph, logits, C.combine(loss, errs)
+
+def outer_process_history(embed_layer):
+    @C.Function
+    def process_history(hist, inp):
+        wk = C.slice(hist, 0, 0, myConfig['wg_dim'])
+        wn = hist[myConfig['wg_dim']:]
+        hist_processed = embed_layer(wk, wn)
+        out_logits = s2smodel(hist_processed, inp)
+        hamax = C.hardmax(out_logits)
+        return hamax
+    return process_history
+
+def create_eval_model(s2smodel, embed_layer, is_test=False):
+    '''
+    return: @input map @softmax @loss
+    '''
+    sentence_end_index = vocabs['</s>']
+    q = C.Axis.new_unique_dynamic_axis('q')
+    a = C.Axis.new_unique_dynamic_axis('a')
+    b = C.Axis.default_batch_axis()
+    qwk = C.sequence.input_variable(myConfig['wg_dim'], sequence_axis = q, is_sparse = False, name='qwk')
+    qwn = C.sequence.input_variable(myConfig['wn_dim'], sequence_axis = q, is_sparse = False, name='qwn')
+    awk = C.sequence.input_variable(myConfig['wg_dim'], sequence_axis = a, is_sparse = False, name='awk')
+    awn = C.sequence.input_variable(myConfig['wn_dim'], sequence_axis = a, is_sparse = False, name='awn')
+
+    input_ph = {'qwk':qwk,'qwn':qwn,'awk':awk,'awn':awn}
+
+    @C.Function
+    def greedy_model(aawk, aawn, qqwk, qqwn):
+        q_onehot = C.splice(qqwk, qqwn)
+        sentence_start = C.sequence.slice(q_onehot, 0, 1)
+
+        @C.Function
+        def process_history(hist, inp):
+            wk = C.slice(hist, 0, 0, myConfig['wg_dim'])
+            wn = hist[myConfig['wg_dim']:]
+            hist_processed = embed_layer(wk, wn)
+            out_logits = s2smodel(hist_processed, inp)
+            hamax = C.hardmax(out_logits)
+            return hamax
+
+        a_processed = embed_layer(aawk, aawn)
+        unfold = UnfoldFrom(lambda history: process_history(history, a_processed),
+                until_predicate=lambda w:w[:sentence_end_index],
+                length_increase=1.5)
+        out_onehot = unfold(sentence_start, a_processed)
+        return out_onehot
+
+
+    q_onehot = C.splice(qwk, qwn)
+    labels = C.sequence.slice(q_onehot,1, 0) # <s> a b c </s> -> a b c </s>
+
+    out_onehot = greedy_model(awk, awn, qwk, qwn)
+    out_onehot = C.reconcile_dynamic_axes(out_onehot, labels)
+    loss = C.cross_entropy_with_softmax(out_onehot, labels)
+    errs = C.classification_error(out_onehot, labels)
+    return input_ph, out_onehot, C.combine(loss, errs)
 
 def create_reader(filename, input_ph, config):
     '''
@@ -121,15 +194,49 @@ def create_reader(filename, input_ph, config):
 def train(config, model):
     max_epoch = config['max_epoch']
     batchsize = config['batchsize']
+    epoch_size = config['epoch_size']
     lr = config['lr']
 
+    # create models
+    embed_layer = GloveEmbed()
 
+    inp_ph ,train_model, loss_errs = create_train_model(model, embed_layer)
+    train_reader, input_map = create_reader('aq_train.ctf', inp_ph, config)
+
+    inp_ph2, greedy_model, loss_errs2 = create_eval_model(model, embed_layer)
+    eval_reader, input_map2 = create_reader('aq_dev.ctf', inp_ph, config)
+
+    # create loggers
+    progress_printer = C.logging.ProgressPrinter(freq=20, tag="Train")
+    tensorboard_writer = C.logging.TensorBoardProgressWriter(50, 'tensorlog', model=train_model)
+
+    lrs = [(1, lr), (5000, lr*0.1), (10000,lr*0.01)]
+    learner = C.fsadagrad(train_model.parameters,
+         #apply the learning rate as if it is a minibatch of size 1
+         lr = C.learning_parameter_schedule(lrs),
+         momentum = C.momentum_schedule(0.9366416204111472, minibatch_size=batchsize),
+         gradient_clipping_threshold_per_sample=2.3,
+         gradient_clipping_with_truncation=True)
+
+    trainer = C.Trainer(train_model, loss_errs, [learner], [progress_printer, tensorboard_writer])
+
+    total_samples = 0
+    for epoch in range(max_epoch):
+        while total_samples < (epoch+1) * epoch_size:
+            # get next minibatch of training data
+            mb_train = train_reader.next_minibatch(batchsize, input_map=input_map)
+            # do the training
+            trainer.train_minibatch(mb_train)
+            total_samples += mb_train[train_reader.streams.awk].num_samples
+
+        if epoch+1 % save_freq == 0:
+            save_name = '{}_{}.model'.format(config['save_name'], epoch+1)
+            print('save {} in {}'.format(save_name, config['output_dir']))
+            trainer.save_checkpoint('output/{}/{}'.format(config['output_dir'], save_name))
 
 def evaluate(model):
     pass
 
 if __name__=='__main__':
-    s2smodel,input_ph = create_model()
-    train_reader, input_map = create_reader('aq_train.ctf' ,input_ph, myConfig)
-    batch = train_reader.next_minibatch(myConfig['batchsize'], input_map)
-    print(batch)
+    s2smodel = create_model()
+    train(myConfig, s2smodel)
