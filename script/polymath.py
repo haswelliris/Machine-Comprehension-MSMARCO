e4c96b3bf730ce0f59fdc687d98ecfa3f3940da6
MintYiqingchen <821244608@qq.com>
2018-04-06 15:15:15 +0800

    word embed file

--- script/polymath.py
+++ script/polymath.py
@@ -15,6 +15,8 @@ class PolyMath:
         self.word_size = data_config['word_size']
         self.abs_path = os.path.dirname(os.path.abspath(__file__))
         pickle_file = os.path.join(self.abs_path, data_config['pickle_file'])
+        self.word_embed_file = data_config['word_embed_file']
+        self.char_embed_file = data_config['char_embed_file']
 
         with open(pickle_file, 'rb') as vf:
             known, self.vocab, self.chars = pickle.load(vf)
@@ -51,7 +53,7 @@ class PolyMath:
     def embed(self):
         # load glove
         npglove = np.zeros((self.wg_dim, self.hidden_dim), dtype=np.float32)
-        with open(os.path.join(self.abs_path, 'glove.6B.100d.txt'), encoding='utf-8') as f:
+        with open(os.path.join(self.abs_path, self.word_embed_file), encoding='utf-8') as f:
             for line in f:
                 parts = line.split()
                 word = parts[0].lower()
@@ -59,7 +61,7 @@ class PolyMath:
                     npglove[self.vocab[word],:] = np.asarray([float(p) for p in parts[1:]])
         glove = C.constant(npglove)
         nonglove = C.parameter(shape=(len(self.vocab) - self.wg_dim, self.hidden_dim), init=C.glorot_uniform(), name='TrainableE')
-        
+
         def func(wg, wn):
             return C.times(wg, glove) + C.times(wn, nonglove)
         return func
@@ -84,10 +86,10 @@ class PolyMath:
         highway = HighwayNetwork(dim=2*self.hidden_dim, highway_layers=self.highway_layers)(embedded)
         highway_drop = C.layers.Dropout(self.dropout)(highway)
         processed = OptimizedRnnStack(self.hidden_dim, bidirectional=True, use_cudnn=self.use_cudnn, name='input_rnn')(highway_drop)
-        
+
         qce = C.one_hot(qc_ph, num_classes=self.c_dim, sparse_output=self.use_sparse)
         cce = C.one_hot(cc_ph, num_classes=self.c_dim, sparse_output=self.use_sparse)
-                
+
         q_processed = processed.clone(C.CloneMethod.share, {input_chars:qce, input_glove_words:qgw_ph, input_nonglove_words:qnw_ph})
         c_processed = processed.clone(C.CloneMethod.share, {input_chars:cce, input_glove_words:cgw_ph, input_nonglove_words:cnw_ph})
 
@@ -96,7 +98,7 @@ class PolyMath:
             [(cgw_ph, cgw),(cnw_ph, cnw),(cc_ph, cc),(qgw_ph, qgw),(qnw_ph, qnw),(qc_ph, qc)],
             'input_layer',
             'input_layer')
-        
+
     def attention_layer(self, context, query):
         q_processed = C.placeholder(shape=(2*self.hidden_dim,))
         c_processed = C.placeholder(shape=(2*self.hidden_dim,))
@@ -125,7 +127,7 @@ class PolyMath:
         q_attn = C.reshape(C.softmax(S), (-1,1))
         #q_attn = print_node(q_attn)
         c2q = C.reshape(C.reduce_sum(C.sequence.broadcast_as(qvw, q_attn) * q_attn, axis=0),(-1))
-        
+
         max_col = C.reduce_max(S)
         c_attn = C.sequence.softmax(max_col)
 
@@ -140,7 +142,7 @@ class PolyMath:
             [(c_processed, context), (q_processed, query)],
             'attention_layer',
             'attention_layer')
-            
+
     def modeling_layer(self, attention_context):
         att_context = C.placeholder(shape=(8*self.hidden_dim,))
         #modeling layer
@@ -156,20 +158,17 @@ class PolyMath:
             [(att_context, attention_context)],
             'modeling_layer',
             'modeling_layer')
-    
+
     def output_layer(self, attention_context, modeling_context):
         att_context = C.placeholder(shape=(8*self.hidden_dim,))
         mod_context = C.placeholder(shape=(2*self.hidden_dim,))
-        #output layer
+        #output layer [#,c][1]
         start_logits = C.layers.Dense(1, name='out_start')(C.dropout(C.splice(mod_context, att_context), self.dropout))
-        if self.two_step:
-            start_hardmax = seq_hardmax(start_logits)
-            att_mod_ctx = C.sequence.last(C.sequence.gather(mod_context, start_hardmax))
-        else:
-            start_prob = C.softmax(start_logits)
-            att_mod_ctx = C.sequence.reduce_sum(mod_context * start_prob)
+
+        start_hardmax = seq_hardmax(start_logits) # [000010000]
+        att_mod_ctx = C.sequence.last(C.sequence.gather(mod_context, start_hardmax)) # [#][2*hidden_dim]
         att_mod_ctx_expanded = C.sequence.broadcast_as(att_mod_ctx, att_context)
-        end_input = C.splice(att_context, mod_context, att_mod_ctx_expanded, mod_context * att_mod_ctx_expanded)
+        end_input = C.splice(att_context, mod_context, att_mod_ctx_expanded, mod_context * att_mod_ctx_expanded) # [#, c][14*hidden_dim]
         m2 = OptimizedRnnStack(self.hidden_dim, bidirectional=True, use_cudnn=self.use_cudnn, name='output_rnn')(end_input)
         end_logits = C.layers.Dense(1, name='out_end')(C.dropout(C.splice(m2, att_context), self.dropout))
 
@@ -197,7 +196,7 @@ class PolyMath:
 
         #input layer
         c_processed, q_processed = self.input_layer(cgw,cnw,cc,qgw,qnw,qc).outputs
-        
+
         # attention layer
         att_context = self.attention_layer(c_processed, q_processed)
 
@@ -213,7 +212,7 @@ class PolyMath:
         #paper_loss = start_loss + end_loss
         new_loss = all_spans_loss(start_logits, ab, end_logits, ae)
         self._model = C.combine([start_logits,end_logits])
-        self._loss = new_loss        
+        self._loss = new_loss
         return self._model, self._loss, self._input_phs
 
     @property
